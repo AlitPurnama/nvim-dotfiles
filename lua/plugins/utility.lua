22b7ed694afb48747ed32e868293452838f26ea4
AlitPurnama <alitpurnama13@gmail.com>
2024-09-09 11:54:49 +0900

    add: luarocks

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -1,5 +1,20 @@
 return {
 	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+		config = true,
+		opts = {
+			rocks = { "magick" },
+		},
+	},
+	{
+		"3rd/image.nvim",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			-- ...
+		end,
+	},
+	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		opts = {
@@ -17,6 +32,66 @@ return {
 			require("nvim-surround").setup({
 				-- Configuration here, or leave empty to use defaults
 			})
+		end,
+	},
+	-- DATABASE
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
+
+	{
+		"andweeb/presence.nvim",
+		opts = {
+			neovim_image_text = "Nah, I'd win",
+			main_image = "file",
+
+			editing_text = "Getting my hands dirty with %s",
+			file_explorer_text = "On a quest through %s",
+			git_commit_text = "Sealing the deal with these changes",
+			plugin_manager_text = "Playing with plugins like a pro",
+			reading_text = "Absorbing knowledge from %s",
+			workspace_text = "Building greatness on %s",
+			line_number_text = "Adventuring through line %s of %s",
+		},
+	},
+	{
+		"mistricky/codesnap.nvim",
+		build = "make build_generator",
+		keys = {
+			{ "<leader>cc", "<cmd>CodeSnap<cr>",     mode = "x", desc = "Save selected code snapshot into clipboard" },
+			{ "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+		},
+		opts = {
+			save_path = "~/Pictures",
+			has_breadcrumbs = true,
+			bg_theme = "bamboo",
+			watermark = "",
+			code_font_family = "JetBrainsMono Nerd Font Mono",
+			bg_padding = 0,
+		},
+	},
+	-- AI
+	{
+		"Exafunction/codeium.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("codeium").setup({})
 		end,
 	},
 }
